6b20bc861b266fd5c4912479e24f83197f0832c8
boscaiolo <boscaiolo.gp@gmail.com>
2019-11-12 13:43:56 +0000

    convert to integration test

--- src/test/java/com/example/patients/PatientApplicationTest.java
+++ src/test/java/com/example/patients/PatientApplicationTest.java
@@ -2,23 +2,34 @@ package com.example.patients;
 
 import com.example.patients.domain.Patient;
 import com.example.patients.repository.PatientRepository;
+import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
-import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class PatientApplicationTest {
+
+    @Autowired
+    private MockMvc mvc;
 
     @Autowired
     private PatientService patientService;
@@ -31,34 +42,40 @@ public class PatientApplicationTest {
     }
 
     @Test
-    public void whenAPatientExistICanRetrieveIt() {
+    public void whenAPatientExistFindReturnsMatchingSinglePatient() throws Exception {
         createSamplePatient();
-        Patient patient = patientService.find("", "").get(0);
-        verifySamplePatient(patient);
-        Optional<Patient> patient1 = patientService.get(patient.getId());
-        verifySamplePatient(patient1.get());
+
+        ResultActions result = mvc.perform(get("/api/")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content()
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+
+        verifySamplePatient(result, "[0]");
     }
 
     @Test
-    @Ignore
-    public void whenAPatientExistICanDeleteIt() {
-        createSamplePatient();
-        Patient patient = patientService.find("", "").get(0);
-        verifySamplePatient(patient);
-        patientService.delete(patient.getId());
-        List<Patient> patients = patientService.find("", "");
-        Assert.assertTrue(patients.isEmpty());
+    public void whenAPatientExistICanRetrievItByID() throws Exception {
+        Patient patient = createSamplePatient();
+
+        ResultActions result = mvc.perform(get("/api/" + patient.getId())
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content()
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+
+        verifySamplePatient(result, "");
     }
 
-    private void verifySamplePatient(Patient patient) {
-        Assert.assertEquals("name", patient.getName());
-        Assert.assertEquals("surname", patient.getSurName());
-        Assert.assertEquals("address", patient.getAddress());
-        Assert.assertEquals("12345", patient.getPhoneNumber());
-        Assert.assertEquals(new Date(1573516800000L), patient.getDob());
+    private void verifySamplePatient(ResultActions result, String offset) throws Exception {
+        result.andExpect(jsonPath("$" + offset + ".name", Matchers.is("name")))
+                .andExpect(jsonPath("$" + offset + ".surName", Matchers.is("surname")))
+                .andExpect(jsonPath("$" + offset + ".address", Matchers.is("address")))
+                .andExpect(jsonPath("$" + offset + ".phoneNumber", Matchers.is("12345")))
+                .andExpect(jsonPath("$" + offset + ".dob", Matchers.is("2019-11-12")));
     }
 
-    private void createSamplePatient(){
+    private Patient createSamplePatient(){
         Patient patient = new Patient();
         patient.setName("name");
         patient.setSurName("surname");
@@ -66,6 +83,6 @@ public class PatientApplicationTest {
         patient.setPhoneNumber("12345");
         patient.setDob(new Date(1573516800000L));
 
-        patientRepository.save(patient);
+        return patientRepository.save(patient);
     }
 }
